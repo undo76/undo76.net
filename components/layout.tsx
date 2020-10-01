30868e3b6809e23d826b4b0ca95da97cf9dd4d74
Manolo Santos <manolo.santos@gmail.com>
2020-10-01 11:56:21 +0200

    Layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,131 +1,32 @@
-export function Layout({ children }) {
+import Link from 'next/link';
+
+export default function Layout({ children }) {
     return (
         <div className="bg-cool-gray-50">
             <nav className="bg-gray-800">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex items-center justify-between h-16">
+                        <h1 className="font-light text-4xl text-red-600 uppercase">
+                            undo76
+                        </h1>
                         <div className="flex items-center">
-                            <div className="flex-shrink-0">
-                                <img
-                                    className="h-8 w-8"
-                                    src="https://tailwindui.com/img/logos/workflow-mark-on-dark.svg"
-                                    alt="Workflow logo"
-                                />
-                            </div>
+
                             <div className="hidden md:block">
                                 <div className="ml-10 flex items-baseline space-x-4">
-                                    <a
-                                        href="#"
-                                        className="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700"
-                                    >
-                                        Dashboard
-                                    </a>
-                                    <a
-                                        href="#"
-                                        className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-                                    >
-                                        Team
-                                    </a>
-                                    <a
-                                        href="#"
-                                        className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-                                    >
-                                        Projects
-                                    </a>
-                                    <a
-                                        href="#"
-                                        className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-                                    >
-                                        Calendar
-                                    </a>
-                                    <a
-                                        href="#"
-                                        className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-                                    >
-                                        Reports
-                                    </a>
+                                    <Link href="/">
+                                        <a className="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700">
+                                            Home
+                                        </a>
+                                    </Link>
+                                    <Link href="/record">
+                                        <a className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">
+                                            About me
+                                        </a>
+                                    </Link>
                                 </div>
                             </div>
                         </div>
-                        <div className="hidden md:block">
-                            <div className="ml-4 flex items-center md:ml-6">
-                                <button
-                                    className="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"
-                                    aria-label="Notifications"
-                                >
-                                    <svg
-                                        className="h-6 w-6"
-                                        stroke="currentColor"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth={2}
-                                            d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                                        />
-                                    </svg>
-                                </button>
-                                {/* Profile dropdown */}
-                                <div className="ml-3 relative">
-                                    <div>
-                                        <button
-                                            className="max-w-xs flex items-center text-sm rounded-full text-white focus:outline-none focus:shadow-solid"
-                                            id="user-menu"
-                                            aria-label="User menu"
-                                            aria-haspopup="true"
-                                        >
-                                            <img
-                                                className="h-8 w-8 rounded-full"
-                                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                                alt="avatar"
-                                            />
-                                        </button>
-                                    </div>
-                                    {/*
-          Profile dropdown panel, show/hide based on dropdown state.
 
-          Entering: "transition ease-out duration-100"
-            From: "transform opacity-0 scale-95"
-            To: "transform opacity-100 scale-100"
-          Leaving: "transition ease-in duration-75"
-            From: "transform opacity-100 scale-100"
-            To: "transform opacity-0 scale-95"
-        */}
-                                    <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg">
-                                        <div
-                                            className="py-1 rounded-md bg-white shadow-xs"
-                                            role="menu"
-                                            aria-orientation="vertical"
-                                            aria-labelledby="user-menu"
-                                        >
-                                            <a
-                                                href="#"
-                                                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                                role="menuitem"
-                                            >
-                                                Your Profile
-                                            </a>
-                                            <a
-                                                href="#"
-                                                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                                role="menuitem"
-                                            >
-                                                Settings
-                                            </a>
-                                            <a
-                                                href="#"
-                                                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                                role="menuitem"
-                                            >
-                                                Sign out
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                         <div className="-mr-2 flex md:hidden">
                             {/* Mobile menu button */}
                             <button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white">
@@ -249,9 +150,7 @@ Open: "block", closed: "hidden"
             </header> */}
             <main>
                 <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-                    {/* Replace with your content */}
-                    {children}
-                    {/* /End replace */}
+                    <div className="px-4 sm:px-0">{children}</div>
                 </div>
             </main>
         </div>
--- pages/record.tsx
+++ pages/record.tsx
@@ -1,34 +1,29 @@
-import { Layout } from "../components/layout";
+import Layout from '../components/layout';
 
 export default function RecordPage() {
     return (
         <Layout>
-            <article className="prose m-auto">
-                <div className="page-body">
-                    <h2 id="fcb01968-2108-47bf-89fa-61d9cea126dc" className="">
-                        CV Summary
-                    </h2>
-                    <p id="d36cd95d-102f-4103-846c-b30d4b6be882" className="">
+            <article className="prose">
+                <div>
+                    <h2>CV Summary</h2>
+                    <p>
                         Manuel Víctor Santos López is an experimented
                         analyst/application architect with good knowledge in Web
                         technologies, Java, XML and Oracle and broad interests
                         in Computer Science, Design, Machine Learning and
                         Software Engineering.
                     </p>
-                    <p id="b061ba45-18a3-4051-8d2a-296842c30b29" className="">
+                    <p>
                         He is a vocational professional and he has got a
                         university degree in IT Engineering, speaks English,
                         French and Spanish and has a very good team spirit. He
                         is self-motivated and is looking forward to learn new
                         things all the time. Specific CS related skills:
                     </p>
-                    <h3 id="3ae70922-5c9d-41f3-a810-c45ca9ca567a" className="">
-                        Programming
-                    </h3>
-                    <ul
-                        id="60e2cbf4-691d-46b9-93d7-13480a3dfe6f"
-                        className="bulleted-list"
-                    >
+
+                    <h3>Programming</h3>
+
+                    <ul>
                         <li>
                             <strong>Foundations:</strong> Algorithms, Data
                             structures, Complexity analysis, Automata/Formal
@@ -36,239 +31,145 @@ export default function RecordPage() {
                             Information theory, Programming paradigms (Object
                             oriented, Functional, Imperative, Declarative).
                         </li>
-                    </ul>
-                    <ul
-                        id="9f89553f-659c-4fdd-b32e-651416f34af1"
-                        className="bulleted-list"
-                    >
+
                         <li>
                             <strong>Languages:</strong> Java, Kotlin,
                             Javascript, Typescript, Python, C/C++11, Ruby.
-                            Mostly forgotten: R, Rust, Haskell, ReasonML/Ocaml,
-                            Prolog, Lisp, etc.
+                            Others: Mostly forgotten: R, Rust, Haskell,
+                            ReasonML/OCaml, Prolog, Lisp, etc.
                         </li>
-                    </ul>
-                    <ul
-                        id="884c8fed-0a09-4e3a-8d4d-90c604be4a58"
-                        className="bulleted-list"
-                    >
+
                         <li>
                             <strong>Web:</strong> HTML, CSS/LESS/SCSS, SVG,
                             Next.js, Webpack, Babel, React, jQuery, RxJS, Redux,
                             KendoUI, Bootstrap.
                         </li>
-                    </ul>
-                    <ul
-                        id="23a0fc21-e1aa-4419-b649-8e1b292c94b9"
-                        className="bulleted-list"
-                    >
+
                         <li>
                             <strong>Frameworks:</strong> Spring (MVC, Data,
                             Security, ...), Ruby on Rails, Django.
                         </li>
-                    </ul>
-                    <ul
-                        id="e7fa05d3-dcf3-4945-aec5-fad62acce605"
-                        className="bulleted-list"
-                    >
+
                         <li>
                             <strong>Databases:</strong> JPA, Hibernate, Oracle,
                             MySQL, Postgres, Redis, Liquibase.
                         </li>
-                    </ul>
-                    <ul
-                        id="83266009-8139-4649-826e-4bbbb02c578b"
-                        className="bulleted-list"
-                    >
+
                         <li>
                             <strong>Version control:</strong> Git, Gitflow,
                             GitHub, SVN, CVS.
                         </li>
-                    </ul>
-                    <ul
-                        id="8d67b35b-ba6e-43a5-87bb-62b39db7f103"
-                        className="bulleted-list"
-                    >
+
                         <li>
                             <strong>Others:</strong> OpenGL, Node.js,
                             Webassembly, GPU computing (CUDA).
                         </li>
                     </ul>
-                    <h3 id="1bc81148-fecf-4939-9aa6-fffce0378ffa" className="">
-                        Architecture and Software development process
-                    </h3>
-                    <ul
-                        id="9124e002-f369-48b0-b6cc-e2abad6be63c"
-                        className="bulleted-list"
-                    >
+
+                    <h3>Architecture and Software development process</h3>
+
+                    <ul>
                         <li>
                             <strong>Methodologies:</strong> RUP, Agile (eXtreme
                             Programming, Scrum), Marte (Telefonica), PM2.
                         </li>
-                    </ul>
-                    <ul
-                        id="c5b7aae2-b99f-46b9-8a9f-3f3b9b6b227c"
-                        className="bulleted-list"
-                    >
+
                         <li>
                             <strong>Architecture:</strong> Domain Driven Design,
-                            Layered arquitecture, Hexagonal/clean architecture,
+                            Layered architecture, Hexagonal/clean architecture,
                             Microservices, Event Sourcing, CQRS, Dependency
                             Injection, Test Driven Development.
                         </li>
-                    </ul>
-                    <ul
-                        id="6d266545-1513-46bd-94cc-408f32aa971a"
-                        className="bulleted-list"
-                    >
+
                         <li>
                             <strong>Security:</strong> OWASP 10, Risk
                             assessment, Fortify.
                         </li>
-                    </ul>
-                    <ul
-                        id="a4711076-a662-460b-a103-8b47c9460600"
-                        className="bulleted-list"
-                    >
+
                         <li>
-                            <strong>CI/CD: </strong>Bamboo, Vercel.
+                            <strong>DevOps/CI/CD: </strong>Bamboo, Vercel.
                         </li>
                     </ul>
-                    <h3 id="bb230a8e-b284-4cfa-9827-5e68d3b9d93d" className="">
-                        OS/Tools
-                    </h3>
-                    <ul
-                        id="06969876-e19f-4a67-99f4-67b97fb5adee"
-                        className="bulleted-list"
-                    >
+
+                    <h3>OS/Administration/Tools</h3>
+
+                    <ul>
                         <li>
                             <strong>Operating Systems: </strong>Windows, Mac
                             OSX, Linux/Unix
                         </li>
-                    </ul>
-                    <ul
-                        id="29ed8005-c1c1-43de-b0bf-fffc65309d95"
-                        className="bulleted-list"
-                    >
+
                         <li>
                             <strong>Containers:</strong> Docker
                         </li>
-                    </ul>
-                    <ul
-                        id="83536956-f375-4823-a8bf-e1867c149f51"
-                        className="bulleted-list"
-                    >
+
                         <li>
                             <strong>Unix/POSIX:</strong> bash, vim, grep, find,
                             make, cat, netcat, ssh, ftp...
                         </li>
-                    </ul>
-                    <ul
-                        id="5c7ff62b-5897-4201-86f9-825d136d1545"
-                        className="bulleted-list"
-                    >
+
                         <li>
                             <strong>IDEs:</strong> VS Code, IntelliJ, Eclipse.
                         </li>
-                    </ul>
-                    <ul
-                        id="930d6eaa-8ea0-41d1-a488-a81f1275a0c6"
-                        className="bulleted-list"
-                    >
+
                         <li>
-                            <strong>MS Office suite:</strong> MS Workd, MS
+                            <strong>MS Office suite: </strong> MS Word, MS
                             Outlook, MS Excel, MS Powerpoint.
                         </li>
-                    </ul>
-                    <ul
-                        id="60c97cf2-d837-47ac-b17b-9a079f98355d"
-                        className="bulleted-list"
-                    >
+
                         <li>
                             <strong>Design:</strong> Gimp, Blender, Pixelmator,
                             Inkscape, Figma, Sketch
                         </li>
-                    </ul>
-                    <ul
-                        id="75414052-a930-4ed4-9bf7-cca2b74d2217"
-                        className="bulleted-list"
-                    >
+
                         <li>
                             <strong>Build tools:</strong> Maven, Gradle, NPM,
                             Ant, Make, Cargo, NPM, Cmake
                         </li>
-                    </ul>
-                    <ul
-                        id="06522ca6-cd58-4091-b82f-41e5182fb4b3"
-                        className="bulleted-list"
-                    >
+
                         <li>
                             <strong>Issue tracking:</strong> JIRA, Trac,
                         </li>
-                    </ul>
-                    <ul
-                        id="3ce40a1a-60be-471f-82f0-c8c69c782496"
-                        className="bulleted-list"
-                    >
+
                         <li>
-                            <strong>Web servers:</strong> Weblogic, Tomcat, inx,
-                            Apache
+                            <strong>Web/application servers: </strong> Weblogic,
+                            Tomcat, Nginx, Apache, NES, SunONE
                         </li>
                     </ul>
-                    <h3 id="6afdc973-a7bd-494d-a3b0-13f83c2de4e7" className="">
-                        Machine Learning, Robotics, Autonomous cars,
-                        Computational graphics
-                    </h3>
-                    <ul
-                        id="ef039541-46b6-4723-a34a-bc3f8cf28bb8"
-                        className="bulleted-list"
-                    >
+
+                    <h3>Machine Learning, Robotics, Autonomous driving, CG</h3>
+
+                    <ul>
                         <li>
-                            <strong>Neural networks and deep learning:</strong>{' '}
+                            <strong>Neural networks and deep learning: </strong>
                             Convolutional, RNN (LSTM &amp; GRU), GANN,
                             Reinforcement learning. Tensorflow, Keras, CAFFE.
                         </li>
-                    </ul>
-                    <ul
-                        id="cf79aaf8-e693-4f1f-9b06-9669750b2311"
-                        className="bulleted-list"
-                    >
+
                         <li>
-                            <strong>Regression/classification/misc.:</strong>{' '}
+                            <strong>Regression/classification/misc.: </strong>
                             Support vector machines, Non linear kernels, PCA,
-                            XGBoost,Naïve Bayes. Sklearn, Numpy, Pandas, Data
+                            XGBoost, Naïve Bayes. Sklearn, Numpy, Pandas, Data
                             visualization.
                         </li>
-                    </ul>
-                    <ul
-                        id="0b818847-dafc-4133-afa9-f60497dc0a1f"
-                        className="bulleted-list"
-                    >
+
                         <li>
                             <strong>
                                 Symbolic/problem solvers/classical AI:
-                            </strong>{' '}
+                            </strong>
                             A*, alpha-pruning, backtracking, graphs, frames,
                             blackboard systems, inference engines (Prolog,
                             LISP).
                         </li>
-                    </ul>
-                    <ul
-                        id="31c74a52-116a-44a9-96f8-7f49ed148e82"
-                        className="bulleted-list"
-                    >
+
                         <li>
-                            <strong>Robotics/Autonomous driving:</strong>{' '}
+                            <strong>Robotics/Autonomous driving: </strong>
                             Planning, Perception, Control (PID, model predictive
                             control), Sensor integration (Kalman filters,
                             particle filters), Dynamic models, System
                             integration (ROS)
                         </li>
-                    </ul>
-                    <ul
-                        id="5671b692-0189-49c7-ac0d-f83972482723"
-                        className="bulleted-list"
-                    >
+
                         <li>
                             <strong>Graphics: </strong>Image processing, OpenGL,
                             OpenCV, Ray-tracing, Shaders, Blender, POV-Ray, etc.
